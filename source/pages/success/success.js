76583a3d5e4d404ce0226ae5f7f175eb21fa09b5
alucard263096 <alucard263096@126.com>
2019-08-02 18:51:20 +0800

    aa

---
+++ source/pages/success/success.js
@@ -0,0 +1,51 @@
+// pages/content/content.js
+import {
+  AppBase
+} from "../../appbase";
+import {
+  ApiConfig
+} from "../../apis/apiconfig";
+import {
+  InstApi
+} from "../../apis/inst.api.js";
+
+class Content extends AppBase {
+  constructor() {
+    super();
+  }
+  onLoad(options) {
+    this.Base.Page = this;
+    //options.id=5;
+    super.onLoad(options);
+  }
+  onMyShow() {
+    var that = this;
+    var instapi = new InstApi();
+
+    instapi.indexbanner({}, (indexbanner) => {
+      this.Base.setMyData({
+        indexbanner
+      });
+    });
+  }
+
+  bindapply() {
+    wx.navigateTo({
+      url: '/pages/changeapply/changeapply',
+    })
+  }
+
+
+  setPageTitle(instinfo) {
+    var title = "订单详情";
+    wx.setNavigationBarTitle({
+      title: title,
+    })
+  }
+}
+var content = new Content();
+var body = content.generateBodyJson();
+body.onLoad = content.onLoad;
+body.onMyShow = content.onMyShow;
+body.bindapply = content.bindapply;
+Page(body)
---
+++ source/pages/waitreceive/waitreceive.js
@@ -0,0 +1,35 @@
+// pages/content/content.js
+import { AppBase } from "../../appbase";
+import { ApiConfig } from "../../apis/apiconfig";
+import { InstApi } from "../../apis/inst.api.js";
+
+class Content extends AppBase {
+  constructor() {
+    super();
+  }
+  onLoad(options) {
+    this.Base.Page = this;
+    //options.id=5;
+    super.onLoad(options);
+  }
+  onMyShow() {
+    var that = this;
+    var instapi = new InstApi();
+t
+    instapi.indexbanner({}, (indexbanner) => {
+      this.Base.setMyData({ indexbanner });
+    });
+  }
+
+  setPageTitle(instinfo) {
+    var title = "订单详情";
+    wx.setNavigationBarTitle({
+      title: title,
+    })
+  }
+}
+var content = new Content();
+var body = content.generateBodyJson();
+body.onLoad = content.onLoad;
+body.onMyShow = content.onMyShow;
+Page(body)
