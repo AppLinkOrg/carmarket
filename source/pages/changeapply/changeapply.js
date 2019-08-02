1d57438af3cd782c9612cf81f3758fff7ca4584a
alucard263096 <alucard263096@126.com>
2019-08-02 10:18:37 +0800

    two

---
+++ source/pages/changeapply/changeapply.js
@@ -0,0 +1,36 @@
+// pages/content/content.js
+import { AppBase } from "../../appbase";
+import { ApiConfig } from "../../apis/apiconfig";
+import { InstApi } from "../../apis/inst.api.js";
+
+class Content extends AppBase {
+  constructor() {
+    super();
+  }
+  onLoad(options) {
+    this.Base.Page = this;
+    //options.id=5;
+    super.onLoad(options);
+  }
+  onMyShow() {
+    var that = this;
+    var instapi = new InstApi();
+
+    instapi.indexbanner({}, (indexbanner) => {
+      this.Base.setMyData({ indexbanner });
+    });
+  }
+
+
+  setPageTitle(instinfo) {
+    var title = "退换货申请";
+    wx.setNavigationBarTitle({
+      title: title,
+    })
+  }
+}
+var content = new Content();
+var body = content.generateBodyJson();
+body.onLoad = content.onLoad;
+body.onMyShow = content.onMyShow;
+Page(body)
